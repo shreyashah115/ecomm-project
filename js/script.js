05e267a0b235c5e00566be812f95da8bfb7d16f4
Shreya <shreyashah115@gmail.com>
2021-04-27 22:18:44 -0400

    Add sort functionality

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const products = [
   {
     name: `MALM`,
     description: `High bed frame/2 storage boxes, Queen`,
-    amount: `356`,
+    amount: 356,
     ratings: 4.5,
     total_ratings: `26`,
     images: [
@@ -16,7 +16,7 @@ const products = [
   {
     name: `HEMNES`,
     description: `Bed frame, Queen`,
-    amount: `578`,
+    amount: 578,
     ratings: 5,
     total_ratings: `81`,
     images: [
@@ -30,7 +30,7 @@ const products = [
   {
     name: `SOMBRE`,
     description: `Upholstered bed frame, Queen`,
-    amount: `159`,
+    amount: 159,
     ratings: 2,
     total_ratings: `6`,
     images: [
@@ -44,7 +44,7 @@ const products = [
   {
     name: `KQITE`,
     description: `Bed frame, Full`,
-    amount: `492`,
+    amount: 492,
     ratings: 4,
     total_ratings: `47`,
     images: [
@@ -58,7 +58,7 @@ const products = [
   {
     name: `IOSEU`,
     description: `High bed, Twin`,
-    amount: `156`,
+    amount: 156,
     ratings: 3.5,
     total_ratings: `9`,
     images: [
@@ -72,7 +72,7 @@ const products = [
   {
     name: `NIESTA`,
     description: `Metal frame bed, Twin`,
-    amount: `98`,
+    amount: 98,
     ratings: 1,
     total_ratings: `1`,
     images: [
@@ -86,7 +86,7 @@ const products = [
   {
     name: `KLAIR`,
     description: `Low Profile Platform Bed, Twin`,
-    amount: `234`,
+    amount: 234,
     ratings: 5,
     total_ratings: `89`,
     images: [
@@ -100,7 +100,7 @@ const products = [
   {
     name: `BUSHWICK`,
     description: `Full bunk bed`,
-    amount: `657`,
+    amount: 657,
     ratings: 3,
     total_ratings: `23`,
     images: [
@@ -114,7 +114,7 @@ const products = [
   {
     name: `ARROW`,
     description: `Twin over full bunk bed`,
-    amount: `356`,
+    amount: 356,
     ratings: 4,
     total_ratings: `12`,
     images: [
@@ -127,94 +127,113 @@ const products = [
   },
 ];
 
+// products.sort((firstItem, secondItem) => firstItem.amount - secondItem.amount);
+
 const fullStar = `<span class="material-icons-round star-rate"> star </span>`;
 const starBorder = `<span class="material-icons-round star-rate"> star_border </span>`;
 const halfStar = `<span class="material-icons-round star-rate"> star_half </span>`;
 
-let section = document.querySelector("#results");
-section.innerHTML = ``;
+const getProducts = () => {
+  console.log("heree=----");
+  let section = document.querySelector("#results");
+  section.innerHTML = ``;
 
-products.forEach((product) => {
-  let index = 0;
-  // let stars = ``;
-  let stars = [];
-  let starCount = 0;
+  products.forEach((product) => {
+    let index = 0;
+    let stars = [];
 
-  if (Number.isInteger(product.ratings)) {
-    while (index < product.ratings) {
-      stars.push(fullStar);
-      index++;
+    if (Number.isInteger(product.ratings)) {
+      while (index < product.ratings) {
+        stars.push(fullStar);
+        index++;
+      }
+    } else {
+      while (index < Math.floor(product.ratings)) {
+        stars.push(fullStar);
+        index++;
+      }
+      stars.push(halfStar);
     }
-  } else {
-    while (index < Math.floor(product.ratings)) {
-      stars.push(fullStar);
-      index++;
+    let starLength = stars.length;
+
+    while (starLength < 5) {
+      stars.push(starBorder);
+      starLength++;
     }
-    stars.push(halfStar);
-  }
-  let starLength = stars.length;
 
-  while (starLength < 5) {
-    stars.push(starBorder);
-    starLength++;
-  }
-
-  let starString = stars.join(``);
-  // console.log(starString);
-  const article = document.createElement(`article`);
-  article.classList.add(`product`);
-  article.innerHTML = `
+    let starString = stars.join(``);
+    const article = document.createElement(`article`);
+    article.classList.add(`product`);
+    article.innerHTML = `
   <header>
-  <a href="product.html">
-    <div class="image-container">
-      <img
-        class="product"
-        srcset="
-          ${product.images[0]}  1920w,
-          ${product.images[0]}   960w,
-          ${product.images[1]}  480w
-        "
-        sizes="(min-width: 1920px) 500px,
-      (min-width: 800px) 500px,
-      480px"
-        src="${product.images[0]}"
-        alt="${product.alt}"
-      />
-      <img
-        class="product-2"
-        srcset="
-          ${product.images[3]} 1920w,
-          ${product.images[3]}  960w
-        "
-        sizes="(min-width: 1920px) 500px,
+    <a href="product.html">
+      <div class="image-container">
+        <img
+          class="product"
+          srcset="
+            ${product.images[0]}  1920w,
+            ${product.images[0]}   960w,
+            ${product.images[1]}  480w
+          "
+          sizes="(min-width: 1920px) 500px,
         (min-width: 800px) 500px,
         480px"
-        src="${product.images[3]}"
-        alt="${product.alt}"
-      />
+          src="${product.images[0]}"
+          alt="${product.alt}"
+        />
+        <img
+          class="product-2"
+          srcset="
+            ${product.images[3]} 1920w,
+            ${product.images[3]}  960w
+          "
+          sizes="(min-width: 1920px) 500px,
+          (min-width: 800px) 500px,
+          480px"
+          src="${product.images[3]}"
+          alt="${product.alt}"
+        />
+      </div>
+    </a>
+
+    <h3>${product.name}</h3>
+    <p>${product.description}</p>
+    <data value="${product.amount}"><strong>$${product.amount}.00</strong></data>
+    <div class="ratings">
+      ${starString}
+      <span class="total-ratings">(${product.total_ratings} ratings)</span>
     </div>
-  </a>
+  </header>
 
-  <h3>${product.name}</h3>
-  <p>${product.description}</p>
-  <data value="${product.amount}"><strong>$${product.amount}.00</strong></data>
-  <div class="ratings">
-${starString}
+  <footer class="product-footer">
+    <button type="button" class="main-button add-cart">
+      <span class="material-icons-round"> add_shopping_cart </span> Add
+      to Cart
+    </button>
+    <button type="button" class="main-button add-wishlist">
+      <span class="material-icons-round add-fav">favorite_border</span>
+      Add to wishlist
+    </button>
+  </footer>`;
 
-    <span class="total-ratings">(${product.total_ratings} ratings)</span>
-  </div>
-</header>
+    section.appendChild(article);
+  });
+};
 
-<footer class="product-footer">
-  <button type="button" class="main-button add-cart">
-    <span class="material-icons-round"> add_shopping_cart </span> Add
-    to Cart
-  </button>
-  <button type="button" class="main-button add-wishlist">
-    <span class="material-icons-round add-fav">favorite_border</span>
-    Add to wishlist
-  </button>
-</footer>`;
+getProducts();
 
-  section.appendChild(article);
+let sort = document.querySelector(`#sort`);
+
+sort.addEventListener("change", (event) => {
+  let selectedSortOption = event.target.value;
+  if (selectedSortOption == `high`) {
+    products.sort(
+      (firstItem, secondItem) => secondItem.amount - firstItem.amount
+    );
+  } else if (selectedSortOption == `low`) {
+    products.sort(
+      (firstItem, secondItem) => firstItem.amount - secondItem.amount
+    );
+  }
+  getProducts();
 });
